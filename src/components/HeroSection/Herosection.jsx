29dfff2b84d9c0db9da5cfac9ad7e2ba5f28bfb2
Duke <mahdina299@gmail.com>
2025-01-24 09:01:23 +0330

    .

--- src/components/HeroSection/Herosection.jsx
+++ src/components/HeroSection/Herosection.jsx
@@ -1,9 +1,13 @@
 import { Box } from "@mui/material";
+import Image from "next/image";
 
 export default function HeroSection({}) {
   return (
-    <Box>
-      Initial commit!
+    <Box sx={{
+      width: "100%",
+      height: "100lvh"
+    }}>
+      <Image src="/img/HeroSection.jpg" fill/>
     </Box>
   )
 } 
